deb70b24d293971ed014629a4ec40a2913eef118
geirb56 <mallegolbrieg@gmail.com>
2026-02-25 22:44:21 +0100

    Create base tester class with common functionality

---
+++ tests/base_tester.py
@@ -0,0 +1,16 @@
+class BaseTester:
+    def __init__(self):
+        # Initialize common properties for all testers
+        pass
+
+    def setup(self):
+        # Setup method common to all testers
+        pass
+
+    def teardown(self):
+        # Teardown method common to all testers
+        pass
+
+    def run_test(self, test_name):
+        # Method to run a test
+        pass
